64c39e88549c7593902679875e574fac22e2224a
Chowdhury Md Rajib Sarwar <md.rajib.sarwar@gmail.com>
2022-07-24 14:16:23 -0400

    Update 3sum.java

--- 3sum.java
+++ 3sum.java
@@ -1,1 +1,24 @@
-
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        HashSet<List<Integer>> set = new HashSet<>();
+        
+        for(int left=0; left<nums.length-2; left++) {
+            int mid = left + 1;
+            int right = nums.length-1;
+            while(mid<right) {
+                int sum = nums[left] + nums[mid] + nums[right];
+                if(sum < 0) {
+                    mid++;
+                } else if(sum > 0) {
+                    right--;
+                } else {
+                    set.add(Arrays.asList(nums[left], nums[mid], nums[right]));
+                    mid++;
+                }
+            }
+        }
+        
+        return new ArrayList<>(set);
+    }
+}
