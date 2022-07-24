cc1a4c1119813aa8e59986508993e5531a4b9b1c
Chowdhury Md Rajib Sarwar <md.rajib.sarwar@gmail.com>
2022-07-24 02:25:39 -0400

    Update container-with-most-water.java

--- container-with-most-water.java
+++ container-with-most-water.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    public int maxArea(int[] height) {
+        int i=0, j=height.length-1;
+        int ans = 0;
+        while(i<j) {
+            if(height[i] > height[j]){
+                ans = Math.max(ans, (j-i) * height[j]);
+                j--;
+            } else {
+                ans = Math.max(ans, (j-i) * height[i]);
+                i++;
+            }
+        }
+        return ans;
+    }
+}
