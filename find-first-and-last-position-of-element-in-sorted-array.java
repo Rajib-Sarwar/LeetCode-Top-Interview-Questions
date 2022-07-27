1d8458e239c68f0abae4c66d903e3db5c1fe6193
Chowdhury Md Rajib Sarwar <md.rajib.sarwar@gmail.com>
2022-07-27 14:28:25 -0400

    Create find-first-and-last-position-of-element-in-sorted-array.java

---
+++ find-first-and-last-position-of-element-in-sorted-array.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int leftIndex = leftMostIndex(nums, 0, nums.length-1, target);
+        
+        if(leftIndex == -1) {
+            return new int[]{-1, -1};
+        }
+        
+        int rightIndex = rightMostIndex(nums, leftIndex, nums.length-1, target);
+        
+        return new int[]{leftIndex, rightIndex};
+        
+    }
+    
+    int leftMostIndex(int[] nums, int left, int right, int target) {
+        int index = -1;
+        while(left<=right) {
+            int mid = (right + left)/2;
+            
+            if(target > nums[mid]) {
+                left = mid + 1;
+            } else if(target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                right = mid - 1;
+                index = mid;
+            }
+        }
+        return index;
+    }
+    
+    int rightMostIndex(int[] nums, int left, int right, int target) {
+        int index = -1;
+        while(left<=right) {
+            int mid = (right + left)/2;
+            
+            if(target > nums[mid]) {
+                left = mid + 1;
+            } else if(target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+                index = mid;
+            }
+        }
+        return index;
+    }
+}
